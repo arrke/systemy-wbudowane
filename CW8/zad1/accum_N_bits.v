030783ef4c793e16de77d7fe57f0b6120226eacf
Arkadiusz Abramowicz <areek99@gmail.com>
2022-05-15 13:53:16 +0200

    CW8 Poprawienie formatowania

--- CW8/zad1/accum_N_bits.v
+++ CW8/zad1/accum_N_bits.v
@@ -1,7 +1,9 @@
-module ripple_carry_adder #(parameter N=4) (input [N-1:0] A,B,
-														  input cin,
-														  output [N-1:0] S,
-														  output cout);
+module ripple_carry_adder #(parameter N=4) 
+(input [N-1:0] A,B,
+input cin,
+output [N-1:0] S,
+output cout);
+
 wire [N-2:0] c;
 generate
 genvar i;
